f7cc9f8b7c0ae30192d29017b14e89ad74a964c1
fatma6622 <125834218+fatma6622@users.noreply.github.com>
2024-04-17 13:27:54 +0200

    GenericRepository

---
+++ Route.Talabat.Core/IRepository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using Route.Talabat.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.IRepository
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		Task<T?> GetAsync(int id);
+		Task<IEnumerable<T>> GetAllAsync();
+	}
+}
---
+++ Route.Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Core.Entities;
+using Route.Talabat.Core.IRepository;
+using Route.Talabat.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Repository
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly StoreContext _dbContext;
+
+		public GenericRepository(StoreContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+        public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync();
+		}
+
+		public async Task<T?> GetAsync(int id)
+		{
+			return await _dbContext.Set<T>().FindAsync(id);
+		}
+	}
+}
