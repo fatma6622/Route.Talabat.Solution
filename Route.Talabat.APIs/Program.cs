0b13d52b8e65967aaef947784e18d9c6e095f17a
fatma6622 <125834218+fatma6622@users.noreply.github.com>
2024-04-17 10:53:16 +0200

    DbContext - ProductConfigrations - Migrations

--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Repository.Data;
+
 namespace Route.Talabat.APIs
 {
 	public class Program
@@ -13,6 +16,11 @@ namespace Route.Talabat.APIs
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
 			webApplicationBuilder.Services.AddSwaggerGen();
+			webApplicationBuilder.Services.AddDbContext<StoreContext>(
+				options => {
+					options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+				}
+			); 
 
 			var app = webApplicationBuilder.Build();
 
---
+++ Route.Talabat.Repository/Data/Config/ProductConfigrations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.Talabat.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Repository.Data.Config
+{
+	public class ProductConfigrations : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.Property(p=>p.Name).IsRequired().HasMaxLength(100);
+			builder.Property(p => p.Description).IsRequired();
+			builder.Property(p => p.PictureUrl).IsRequired();
+			builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+			builder.HasOne(p => p.Brand).WithMany().HasForeignKey(p => p.BrandId);
+			builder.HasOne(p => p.Category).WithMany().HasForeignKey(p => p.CategoryId);
+		}
+	}
+}
